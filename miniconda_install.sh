d7d1ca7f30a9e0b9120bf3f50d135c1619cffa56
XXXXRT666 <157766680+XXXXRT666@users.noreply.github.com>
2025-07-07 20:24:53 +0800

    .

--- miniconda_install.sh
+++ miniconda_install.sh
@@ -62,6 +62,7 @@ elif [ "$CUDA_VERSION" = "12.6" ]; then
     "$HOME/miniconda3/bin/pip" install torch torchaudio --no-cache-dir --index-url https://download.pytorch.org/whl/cu126
     "$HOME/miniconda3/bin/conda" install cuda-nvcc=12.6 -c nvidia
 fi
+source "$HOME/miniconda3/etc/profile.d/conda.sh"
 echo "[INFO]: $(which nvcc)"
 "$HOME/miniconda3/bin/pip" install 'git+https://github.com/Dao-AILab/flash-attention.git@v2.7.4.post1#egg=flash-attn'
 
