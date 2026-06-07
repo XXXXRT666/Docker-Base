e0c98eb9a8a899d738f6ff5e9f9319e41dc28aec
XXXXRT666 <157766680+XXXXRT666@users.noreply.github.com>
2026-06-07 21:29:17 +0800

    .

--- miniforge_install.sh
+++ miniforge_install.sh
@@ -64,7 +64,7 @@ cd workspace
 "$HOME/conda/bin/uv" pip install opencc pyopenjtalk --no-cache-dir --python "$HOME/conda/bin/python"
 
 if [ "$CUDA_VERSION" = "12.8" ]; then
-    "$HOME/conda/bin/uv" pip install ".[cu128]" -U --no-cache-dir --python "$HOME/conda/bin/python" -v
+    "$HOME/conda/bin/uv" pip install ".[cu128]" -U --no-cache-dir --python "$HOME/conda/bin/python"
 elif [ "$CUDA_VERSION" = "12.6" ]; then
     "$HOME/conda/bin/uv" pip install ".[cu126]" -U --no-cache-dir --python "$HOME/conda/bin/python"
 fi
